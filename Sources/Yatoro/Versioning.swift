38b7c237470d94ddefd4095bea48d65a47b34edd
Herman Berdnikov <applejay1802@gmail.com>
2024-11-05 23:31:02 -0500

    Start 0.1.1 development

--- Sources/Yatoro/Versioning.swift
+++ Sources/Yatoro/Versioning.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-public let yatoroVersionCore: String = "0.1.0"
+public let yatoroVersionCore: String = "0.1.1"
 
-fileprivate let readyForRelease: Bool = true
+fileprivate let readyForRelease: Bool = false
 
 public let yatoroBuildUUID = UUID().uuidString
 
