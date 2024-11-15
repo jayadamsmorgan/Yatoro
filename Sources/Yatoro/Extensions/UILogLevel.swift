1f267ecf89665c7073539163a7c26a4e40c0667b
Herman Berdnikov <applejay1802@gmail.com>
2024-11-15 09:44:43 -0500

    UILogLevel custom ExpressibleByArgument

--- Sources/Yatoro/Extensions/UILogLevel.swift
+++ Sources/Yatoro/Extensions/UILogLevel.swift
@@ -2,5 +2,20 @@ import ArgumentParser
 import SwiftNotCurses
 
 extension UILogLevel: ExpressibleByArgument {
-    // TODO: customExpressible
+
+    public init?(argument: String) {
+        switch argument.lowercased() {
+        case "info": self = .info
+        case "debug": self = .debug
+        case "error": self = .error
+        case "fatal": self = .fatal
+        case "panic": self = .panic
+        case "trace": self = .trace
+        case "silent": self = .silent
+        case "verbose": self = .verbose
+        case "warning": self = .warning
+        default: return nil
+        }
+    }
+
 }
