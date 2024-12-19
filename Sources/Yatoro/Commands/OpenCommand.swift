d5c81379019969f72de59ede000be718831f24a3
Herman Berdnikov <applejay1802@gmail.com>
2024-12-19 18:39:19 -0500

    Fix opening detail pages in recently played searches

--- Sources/Yatoro/Commands/OpenCommand.swift
+++ Sources/Yatoro/Commands/OpenCommand.swift
@@ -145,134 +145,30 @@ struct OpenCommand: AsyncParsableCommand {
             }
 
             switch musicItem {
-            case var songItem as Song:
-                songItem = try await songItem.with([.albums, .artists])
-                let songDescription = SongDescriptionResult(
-                    song: songItem,
-                    artists: songItem.artists,
-                    album: songItem.albums?.first
-                )
-                SearchManager.shared.lastSearchResult = .init(
-                    previous: SearchManager.shared.lastSearchResult,
-                    .songDescription(songDescription),
-                    inPlace: false
-                )
-            case var albumItem as Album:
-                albumItem = try await albumItem.with([.tracks, .artists])
-                var songs: [Song] = []
-                if let tracks = albumItem.tracks {
-                    for track in tracks {
-                        switch track {
-                        case .song(let song): songs.append(song)
-                        case .musicVideo(_): break
-                        @unknown default: break
-                        }
-                    }
-                }
-                let albumDescription = AlbumDescriptionResult(
-                    album: albumItem,
-                    songs: MusicItemCollection(songs),
-                    artists: albumItem.artists
-                )
-                SearchManager.shared.lastSearchResult = .init(
-                    previous: SearchManager.shared.lastSearchResult,
-                    .albumDescription(albumDescription),
-                    inPlace: false
-                )
-            case var artistItem as Artist:
-                artistItem = try await artistItem.with([.topSongs, .albums])
-                let artistDescription = ArtistDescriptionResult(
-                    artist: artistItem,
-                    topSongs: artistItem.topSongs,
-                    lastAlbums: artistItem.albums
-                )
-                SearchManager.shared.lastSearchResult = .init(
-                    previous: SearchManager.shared.lastSearchResult,
-                    .artistDescription(artistDescription),
-                    inPlace: false
-                )
-            case var playlistItem as Playlist:
-                playlistItem = try await playlistItem.with([.tracks])
-                var songs: [Song] = []
-                if let tracks = playlistItem.tracks {
-                    for track in tracks {
-                        switch track {
-                        case .song(let song):
-                            songs.append(song)
-                        case .musicVideo(_): break
-                        @unknown default: break
-                        }
-                    }
-                }
-                let playlistDescription = PlaylistDescriptionResult(
-                    playlist: playlistItem,
-                    songs: MusicItemCollection(songs)
-                )
-                SearchManager.shared.lastSearchResult = .init(
-                    previous: SearchManager.shared.lastSearchResult,
-                    .playlistDescription(playlistDescription),
-                    inPlace: command.inPlace
-                )
+
+            case let songItem as Song:
+                try await openSong(songItem)
+
+            case let albumItem as Album:
+                try await openAlbum(albumItem)
+
+            case let artistItem as Artist:
+                try await openArtist(artistItem)
+
+            case let playlistItem as Playlist:
+                try await openPlaylist(playlistItem, inPlace: command.inPlace)
+
             case _ as Station:
                 await executionError("Error: Not yet implemented")
 
             case let recommendationItem as MusicPersonalRecommendation:
-                var playlists: MusicItemCollection<Playlist>?
-                var stations: MusicItemCollection<Station>?
-                var albums: MusicItemCollection<Album>?
-                // Sometimes the recommendationItem is received with 'tracks' property,
-                // Sometimes it's received containing playlists, stations and albums as a separate properties...
-                // That's how Apple Music API works... :(
-                if recommendationItem.items.isEmpty {
-                    for type in recommendationItem.types {
-                        switch type {
-                        case is Playlist.Type:
-                            // TODO: Probably check if playlists are empty anyway, maybe that happens, same for other types
-                            playlists = recommendationItem.playlists
-                        case is Album.Type:
-                            albums = recommendationItem.albums
-                        case is Station.Type:
-                            stations = recommendationItem.stations
-                        default: break
-                        }
-                    }
-                } else {
-                    var playlistsArr: [Playlist] = []
-                    var stationsArr: [Station] = []
-                    var albumsArr: [Album] = []
-                    for item in recommendationItem.items {
-                        switch item {
-                        case .album(let album):
-                            albumsArr.append(album)
-                        case .station(let station):
-                            stationsArr.append(station)
-                        case .playlist(let playlist):
-                            playlistsArr.append(playlist)
-                        @unknown default: break
-                        }
-                    }
-                    if !playlistsArr.isEmpty {
-                        playlists = MusicItemCollection(playlistsArr)
-                    }
-                    if !stationsArr.isEmpty {
-                        stations = MusicItemCollection(stationsArr)
-                    }
-                    if !albumsArr.isEmpty {
-                        albums = MusicItemCollection(albumsArr)
-                    }
-                }
-                let recommendationDescription = RecommendationDescriptionResult(
-                    recommendation: recommendationItem,
-                    albums: albums,
-                    stations: stations,
-                    playlists: playlists
-                )
-                SearchManager.shared.lastSearchResult = .init(
-                    previous: SearchManager.shared.lastSearchResult,
-                    .recommendationDescription(recommendationDescription),
-                    inPlace: false
-                )
-            default: break
+                try await openRecommendation(recommendationItem)
+
+            case let recentlyPlayedItem as RecentlyPlayedMusicItem:
+                try await openRecentlyPlayed(recentlyPlayedItem, inPlace: command.inPlace)
+
+            default:
+                logger?.error("OpenCommand: Unknown type of the musicItem")
             }
 
         } catch {
@@ -285,6 +181,159 @@ struct OpenCommand: AsyncParsableCommand {
                     await executionError("Error: wrong arguments")
                 }
             }
+        }
+    }
+
+    @MainActor
+    private static func openSong(_ songItem: Song) async throws {
+        let songItem = try await songItem.with([.albums, .artists])
+        let songDescription = SongDescriptionResult(
+            song: songItem,
+            artists: songItem.artists,
+            album: songItem.albums?.first
+        )
+        SearchManager.shared.lastSearchResult = .init(
+            previous: SearchManager.shared.lastSearchResult,
+            .songDescription(songDescription),
+            inPlace: false
+        )
+    }
+
+    @MainActor
+    private static func openPlaylist(_ playlistItem: Playlist, inPlace: Bool) async throws {
+        let playlistItem = try await playlistItem.with([.tracks])
+        var songs: [Song] = []
+        if let tracks = playlistItem.tracks {
+            for track in tracks {
+                switch track {
+                case .song(let song):
+                    songs.append(song)
+                case .musicVideo(_): break
+                @unknown default: break
+                }
+            }
+        }
+        let playlistDescription = PlaylistDescriptionResult(
+            playlist: playlistItem,
+            songs: MusicItemCollection(songs)
+        )
+        SearchManager.shared.lastSearchResult = .init(
+            previous: SearchManager.shared.lastSearchResult,
+            .playlistDescription(playlistDescription),
+            inPlace: inPlace
+        )
+    }
+
+    @MainActor
+    private static func openAlbum(_ albumItem: Album) async throws {
+        let albumItem = try await albumItem.with([.tracks, .artists])
+        var songs: [Song] = []
+        if let tracks = albumItem.tracks {
+            for track in tracks {
+                switch track {
+                case .song(let song): songs.append(song)
+                case .musicVideo(_): break
+                @unknown default: break
+                }
+            }
+        }
+        let albumDescription = AlbumDescriptionResult(
+            album: albumItem,
+            songs: MusicItemCollection(songs),
+            artists: albumItem.artists
+        )
+        SearchManager.shared.lastSearchResult = .init(
+            previous: SearchManager.shared.lastSearchResult,
+            .albumDescription(albumDescription),
+            inPlace: false
+        )
+    }
+
+    @MainActor
+    private static func openArtist(_ artistItem: Artist) async throws {
+        let artistItem = try await artistItem.with([.topSongs, .albums])
+        let artistDescription = ArtistDescriptionResult(
+            artist: artistItem,
+            topSongs: artistItem.topSongs,
+            lastAlbums: artistItem.albums
+        )
+        SearchManager.shared.lastSearchResult = .init(
+            previous: SearchManager.shared.lastSearchResult,
+            .artistDescription(artistDescription),
+            inPlace: false
+        )
+    }
+
+    @MainActor
+    private static func openRecommendation(_ recommendationItem: MusicPersonalRecommendation) async throws {
+        var playlists: MusicItemCollection<Playlist>?
+        var stations: MusicItemCollection<Station>?
+        var albums: MusicItemCollection<Album>?
+        // Sometimes the recommendationItem is received with 'tracks' property,
+        // Sometimes it's received containing playlists, stations and albums as a separate properties...
+        // That's how Apple Music API works... :(
+        if recommendationItem.items.isEmpty {
+            for type in recommendationItem.types {
+                switch type {
+                case is Playlist.Type:
+                    // TODO: Probably check if playlists are empty anyway, maybe that happens, same for other types
+                    playlists = recommendationItem.playlists
+                case is Album.Type:
+                    albums = recommendationItem.albums
+                case is Station.Type:
+                    stations = recommendationItem.stations
+                default: break
+                }
+            }
+        } else {
+            var playlistsArr: [Playlist] = []
+            var stationsArr: [Station] = []
+            var albumsArr: [Album] = []
+            for item in recommendationItem.items {
+                switch item {
+                case .album(let album):
+                    albumsArr.append(album)
+                case .station(let station):
+                    stationsArr.append(station)
+                case .playlist(let playlist):
+                    playlistsArr.append(playlist)
+                @unknown default: break
+                }
+            }
+            if !playlistsArr.isEmpty {
+                playlists = MusicItemCollection(playlistsArr)
+            }
+            if !stationsArr.isEmpty {
+                stations = MusicItemCollection(stationsArr)
+            }
+            if !albumsArr.isEmpty {
+                albums = MusicItemCollection(albumsArr)
+            }
+        }
+        let recommendationDescription = RecommendationDescriptionResult(
+            recommendation: recommendationItem,
+            albums: albums,
+            stations: stations,
+            playlists: playlists
+        )
+        SearchManager.shared.lastSearchResult = .init(
+            previous: SearchManager.shared.lastSearchResult,
+            .recommendationDescription(recommendationDescription),
+            inPlace: false
+        )
+    }
+
+    @MainActor
+    private static func openRecentlyPlayed(_ recentlyPlayedItem: RecentlyPlayedMusicItem, inPlace: Bool) async throws {
+        switch recentlyPlayedItem {
+        case .album(let albumItem):
+            try await openAlbum(albumItem)
+        case .station(_):
+            await executionError("Error: Not yet implemented")
+        case .playlist(let playlistItem):
+            try await openPlaylist(playlistItem, inPlace: inPlace)
+        @unknown default:
+            logger?.error("OpenCommand: Unknown type of recentlyPlayedItem")
         }
     }
 
